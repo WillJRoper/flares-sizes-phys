5d9b4d3f6e40f2316e2ec938f305f58afedacd72
wjr21 <wjr21@sussex.ac.uk>
2022-04-20 18:51:43 +0100

    automatically make directories

--- hmrs.py
+++ hmrs.py
@@ -181,4 +181,4 @@ def plot_stellar_hmr(sim, regions, snap, weight_norm):
     ax.set_ylabel("$R_{1/2} / [\mathrm{pkpc}]$")
 
     # Save figure
-    fig.savefig("/plots/stellar_hmr_%s.png")
+    fig.savefig("plots/stellar_hmr_%s.png")
