7b070301a77f98a6536ee4762968cecd0c2957b1
wjr21 <wjr21@sussex.ac.uk>
2022-05-20 17:51:54 +0100

    hmr physic comp plot

--- phys_comp.py
+++ phys_comp.py
@@ -134,3 +134,62 @@ def plot_birth_met_evo():
     mkdir("plots/physics_vary/")
     fig.savefig("plots/physics_vary/stellar_met_%s.png" % snap,
                 bbox_inches="tight")
+
+
+def plot_hmr_phys_comp(snap):
+
+    # Define the path
+    path = "/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/<type>/data/"
+
+    # Define physics variations directories
+    types = ["G-EAGLE_00", "FLARES_00_REF", "FLARES_00_highFBlim",
+             "FLARES_00_medFBlim", "FLARES_00_slightFBlim",
+             "FLARES_00_instantFB", "FLARES_00_noZSFthresh"]
+
+    # Define labels for each
+    labels = ["AGNdT9", "REF", "$f_{\mathrm{th, max}}=10$",
+              "$f_{\mathrm{th, max}}=6$", "$f_{\mathrm{th, max}}=4$",
+              "InstantFB", "$n_{H}^{*}=0$"]
+
+    # Define linestyles
+    linestyles = ["-", "-", "--", "--", "--", "dotted", "dotted"]
+
+    # Set up the plot
+    fig = plt.figure(figsize=(3.5, 3.5))
+    ax = fig.add_subplot(111)
+
+    # Log the y axis
+    ax.loglog()
+
+    # Loop over the variants
+    for t, l, ls in zip(types, labels, linestyles):
+
+        # Get the arrays from the raw data files
+        hmr = eagle_io.read_array('SUBFIND', path.replace("<type>", t),
+                                  snap,
+                                  'Subhalo/HalfMassRad',
+                                  noH=True, physicalUnits=True,
+                                  numThreads=8)[:, 4] * 1000
+        mass = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+                                   snap,
+                                   "Subhalo/ApertureMeasurements/Mass/030kpc",
+                                   noH=True, physicalUnits=True,
+                                   numThreads=8)[:, 4] * 10 ** 10
+
+        # Plot median curves
+        okinds = mass > 0
+        plot_meidan_stat(mass[okinds], hmr[okinds], np.ones(hmr[okinds].size),
+                         ax, lab=l, color=None, ls=ls)
+
+    # Label axes
+    ax.set_ylabel(r"$Z_{\mathrm{birth}}$")
+    ax.set_xlabel(r"$z_{\mathrm{birth}}$")
+
+    ax.legend(loc='upper center',
+              bbox_to_anchor=(0.5, -0.35),
+              fancybox=True, ncol=3)
+
+    # Save figure
+    mkdir("plots/physics_vary/")
+    fig.savefig("plots/physics_vary/stellar_hmr_%s.png" % snap,
+                bbox_inches="tight")
