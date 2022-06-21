863e7edf3deabe29d36243c9bd783a07c54f7699
wjr21 <wjr21@sussex.ac.uk>
2022-06-21 17:36:37 +0100

    Setting limits on z calculation

--- stellar_properties.py
+++ stellar_properties.py
@@ -341,7 +341,8 @@ def plot_sfr_evo(stellar_data, snap):
         plt_zs = []
         for z_low in flares_z_bins[:-1]:
 
-            z_high = z_at_value(cosmo.age, cosmo.age(z_low) - (100 * u.Myr))
+            z_high = z_at_value(cosmo.age, cosmo.age(z_low) - (100 * u.Myr),
+                                zmin=0, zmax=50)
 
             zokinds = np.logical_and(np.logical_and(zs < z_high, zs >= z_low),
                                      okinds)
