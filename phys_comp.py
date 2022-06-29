24b07a5fe319144ed64cc0e5d151e1b7a2bb93b8
wjr21 <wjr21@sussex.ac.uk>
2022-06-29 18:57:05 +0100

    Reducing time window to 10 Myrs

--- phys_comp.py
+++ phys_comp.py
@@ -749,7 +749,7 @@ def plot_potential(snap):
             zs = star_data[key]['PartType4/StellarFormationTime']
             z_low = z_at_value(cosmo.age, cosmo.age(z) - (0.03 * u.Gyr),
                                zmin=0, zmax=50)
-            z_high = z_at_value(cosmo.age, cosmo.age(z) - (0.06 * u.Gyr),
+            z_high = z_at_value(cosmo.age, cosmo.age(z) - (0.04 * u.Gyr),
                                 zmin=0, zmax=50)
             zokinds = np.logical_and(zs < z_high, zs >= z_low)
             ini_ms = np.array(
