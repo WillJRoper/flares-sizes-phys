32f31edaa094d48e3114a06893a5f3dd1ad9bf0f
wjr21 <wjr21@sussex.ac.uk>
2022-08-15 15:05:42 +0100

    Fixing ax scale

--- graph_plots.py
+++ graph_plots.py
@@ -207,7 +207,7 @@ def plot_size_change(stellar_data, snaps, plt_type, weight_norm):
     im = ax.hexbin(tot_cont, delta_hmr,  gridsize=30,
                    mincnt=np.min(w) - (0.1 * np.min(w)),
                    C=w, extent=[-1, 1.3, -1, 1.3],
-                   reduce_C_function=np.sum, xscale="log",
+                   reduce_C_function=np.sum,
                    linewidths=0.2, norm=weight_norm)
 
     # Axes labels
