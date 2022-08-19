764493241f58b99fe132b1e974d2f7c8c075fc17
wjr21 <wjr21@sussex.ac.uk>
2022-08-19 20:17:12 +0100

    Fixing variable name

--- graph_plots.py
+++ graph_plots.py
@@ -2747,9 +2747,9 @@ def plot_size_change_starpos(stellar_data, snaps, weight_norm):
             s_pids = master_s_pids[s_start: s_start + s_len]
 
             # Get the particles present in the previous snapshot
-            _, _, part_in_commmon = np.intersect1d(prog_s_pids, s_pids,
-                                                   assume_unique=True,
-                                                   return_indices=True)
+            _, _, parts_in_commmon = np.intersect1d(prog_s_pids, s_pids,
+                                                    assume_unique=True,
+                                                    return_indices=True)
 
             # Calculate radius and apply a 30 pkpc aperture
             rs = np.sqrt((coords[parts_in_common, 0] - cop[0]) ** 2
