6e291632d909e70a75120ca937134b4c750d1a28
wjr21 <wjr21@sussex.ac.uk>
2022-09-01 18:08:37 +0100

    Typo

--- phys_comp.py
+++ phys_comp.py
@@ -1031,15 +1031,16 @@ def plot_birth_met_vary(stellar_data, snap, path):
 
             if j == i:
                 im = axes[i, j].hexbin(hex_dict[ti]["zs"], hex_dict[ti]["mets"],
-                                      mincnt=1, gridsize=50, linewidth=0.2,
-                                      cmap="plasma", norm=norm, extent=extent)
+                                       mincnt=1, gridsize=50, linewidth=0.2,
+                                       cmap="plasma", norm=norm, extent=extent)
                 # Set up colorbar
                 cbar = fig.colorbar(im, cax1)
                 cbar.set_label("$N$")
             else:
-                im = axes[i, j.hexbin(hex_dict[ti]["zs"], hex_dict[ti]["mets"], gridsize=50, linewidth=0.2,
-                                      cmap="plasma", norm=resi_norm,
-                                      extent=extent)
+                im = axes[i, j].hexbin(hex_dict[ti]["zs"], hex_dict[ti]["mets"],
+                                       gridsize=50, linewidth=0.2,
+                                       cmap="plasma", norm=resi_norm,
+                                       extent=extent)
                 im.set_array(hex_dict[ti]["h"] - hex_dict[tj]["h"])
                 # Set up colorbar
                 cbar = fig.colorbar(im, cax2)
@@ -1050,41 +1051,40 @@ def plot_birth_met_vary(stellar_data, snap, path):
     plt.close(fig)
 
 
-
 def plot_birth_den_vary(stellar_data, snap, path):
 
     # Define the path
-    ini_path= "/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/<type>/data/"
+    ini_path = "/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/<type>/data/"
 
     # Define physics variations directories
-    types= ["flares_00", "FLARES_00_REF",
+    types = ["flares_00", "FLARES_00_REF",
              "FLARES_00_instantFB", "FLARES_00_noZSFthresh",
              "FLARES_00_slightFBlim", "FLARES_00_medFBlim",
              "FLARES_00_highFBlim"]
 
     # Define labels for each
-    labels= ["AGNdT9", "REF", "SKIP",
+    labels = ["AGNdT9", "REF", "SKIP",
               "InstantFB", "$Z^0$", "SKIP",
               "$f_{\mathrm{th, max}}=4$", "$f_{\mathrm{th, max}}=6$",
               "$f_{\mathrm{th, max}}=10$"]
 
     # Define plot dimensions
-    nrows= 3
-    ncols= 3
+    nrows = 3
+    ncols = 3
 
     # Define norm
-    norm= LogNorm(vmin=1, vmax=50000)
+    norm = LogNorm(vmin=1, vmax=50000)
 
     # Define hexbin extent
-    extent= [4.6, 22, -2.2, 5.5]
+    extent = [4.6, 22, -2.2, 5.5]
 
     # Set up the plot
-    fig= plt.figure(figsize=(nrows * 3.5, ncols * 3.5))
-    gs= gridspec.GridSpec(nrows=nrows, ncols=ncols + 1,
+    fig = plt.figure(figsize=(nrows * 3.5, ncols * 3.5))
+    gs = gridspec.GridSpec(nrows=nrows, ncols=ncols + 1,
                            width_ratios=[20, ] * ncols + [1, ])
     gs.update(wspace=0.0, hspace=0.0)
-    axes= []
-    cax= fig.add_subplot(gs[-1, -1])
+    axes = []
+    cax = fig.add_subplot(gs[-1, -1])
 
     for i in range(nrows):
         for j in range(ncols):
@@ -1096,7 +1096,7 @@ def plot_birth_den_vary(stellar_data, snap, path):
                 continue
 
             # Create axis
-            ax= fig.add_subplot(gs[i, j])
+            ax = fig.add_subplot(gs[i, j])
             ax.semilogy()
 
             # Include labels
@@ -1128,23 +1128,23 @@ def plot_birth_den_vary(stellar_data, snap, path):
 
     for (ind, t), l in zip(enumerate(types), labels):
 
-        path= ini_path.replace("<type>", t)
+        path = ini_path.replace("<type>", t)
 
         print(path)
 
-        reg_zs, reg_dens= get_nonmaster_evo_data(
+        reg_zs, reg_dens = get_nonmaster_evo_data(
             path, snap, y_key="PartType4/BirthDensity")
 
         # Convert density to hydrogen number density
-        reg_dens= (reg_dens * 10**10
+        reg_dens = (reg_dens * 10**10
                     * Msun / Mpc ** 3 / mh).to(1 / cm ** 3).value
 
-        im= axes[ind].hexbin(reg_zs, reg_dens, mincnt=1, gridsize=50,
+        im = axes[ind].hexbin(reg_zs, reg_dens, mincnt=1, gridsize=50,
                               yscale="log", linewidth=0.2, cmap="plasma",
                               norm=norm, extent=extent)
 
     # Set up colorbar
-    cbar= fig.colorbar(im, cax)
+    cbar = fig.colorbar(im, cax)
     cbar.set_label("$N$")
 
     # Save figure
@@ -1158,52 +1158,52 @@ def plot_birth_den_vary(stellar_data, snap, path):
 def plot_ssfr_mass_vary(snap):
 
     # Define redshift
-    z= float(snap.split("z")[-1].replace("p", "."))
+    z = float(snap.split("z")[-1].replace("p", "."))
 
     # What redshift was 100 Myrs ago?
-    z_100= z_at_value(cosmo.age, cosmo.age(z) - (0.1 * u.Gyr),
+    z_100 = z_at_value(cosmo.age, cosmo.age(z) - (0.1 * u.Gyr),
                        zmin=0, zmax=50)
 
     # Define the path
-    ini_path= "/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/<type>/data/"
+    ini_path = "/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/<type>/data/"
 
     # Define physics variations directories
-    types= ["flares_00", "FLARES_00_REF",
+    types = ["flares_00", "FLARES_00_REF",
              "FLARES_00_instantFB", "FLARES_00_noZSFthresh",
              "FLARES_00_slightFBlim", "FLARES_00_medFBlim",
              "FLARES_00_highFBlim"]
 
     # Define labels for each
-    labels= ["AGNdT9", "REF", "SKIP",
+    labels = ["AGNdT9", "REF", "SKIP",
               "InstantFB", "$Z^0$", "SKIP",
               "$f_{\mathrm{th, max}}=4$",
               "$f_{\mathrm{th, max}}=6$",
               "$f_{\mathrm{th, max}}=10$"]
 
     # Define plot dimensions
-    nrows= 3
-    ncols= 3
+    nrows = 3
+    ncols = 3
 
     # Define norm
-    norm= LogNorm(vmin=1, vmax=10)
+    norm = LogNorm(vmin=1, vmax=10)
 
     # Define hexbin extent
-    extent= [8, 11.5, 0, 15]
-    extent1= [-1.5, 1.5, 0, 15]
+    extent = [8, 11.5, 0, 15]
+    extent1 = [-1.5, 1.5, 0, 15]
 
     # Set up the plots
-    fig= plt.figure(figsize=(nrows * 3.5, ncols * 3.5))
-    fig1= plt.figure(figsize=(nrows * 3.5, ncols * 3.5))
-    gs= gridspec.GridSpec(nrows=nrows, ncols=ncols + 1,
+    fig = plt.figure(figsize=(nrows * 3.5, ncols * 3.5))
+    fig1 = plt.figure(figsize=(nrows * 3.5, ncols * 3.5))
+    gs = gridspec.GridSpec(nrows=nrows, ncols=ncols + 1,
                            width_ratios=[20, ] * ncols + [1, ])
     gs.update(wspace=0.0, hspace=0.0)
-    gs1= gridspec.GridSpec(nrows=nrows, ncols=ncols + 1,
+    gs1 = gridspec.GridSpec(nrows=nrows, ncols=ncols + 1,
                             width_ratios=[20, ] * ncols + [1, ])
     gs1.update(wspace=0.0, hspace=0.0)
-    axes= []
-    cax= fig.add_subplot(gs[-1, -1])
-    axes1= []
-    cax1= fig1.add_subplot(gs1[-1, -1])
+    axes = []
+    cax = fig.add_subplot(gs[-1, -1])
+    axes1 = []
+    cax1 = fig1.add_subplot(gs1[-1, -1])
 
     for i in range(nrows):
         for j in range(ncols):
@@ -1215,8 +1215,8 @@ def plot_ssfr_mass_vary(snap):
                 continue
 
             # Create axis
-            ax= fig.add_subplot(gs[i, j])
-            ax1= fig1.add_subplot(gs1[i, j])
+            ax = fig.add_subplot(gs[i, j])
+            ax1 = fig1.add_subplot(gs1[i, j])
 
             # Include labels
             if j == 0:
@@ -1261,56 +1261,56 @@ def plot_ssfr_mass_vary(snap):
 
     for (ind, t), l in zip(enumerate(types), labels):
 
-        path= ini_path.replace("<type>", t)
+        path = ini_path.replace("<type>", t)
 
         print(path)
 
-        mass= eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+        mass = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
                                    snap,
                                    "Subhalo/ApertureMeasurements/Mass/030kpc",
                                    noH=True, physicalUnits=True,
                                    numThreads=8)[:, 4] * 10 ** 10
-        hmrs= eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+        hmrs = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
                                    snap,
                                    "Subhalo/HalfMassRad",
                                    noH=True, physicalUnits=True,
                                    numThreads=8)[:, 4] * 10 ** 3
-        cops= eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+        cops = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
                                    snap,
                                    "Subhalo/CentreOfPotential",
                                    noH=True, physicalUnits=True,
                                    numThreads=8) * 1000
-        grps= eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+        grps = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
                                    snap,
                                    "Subhalo/GroupNumber",
                                    noH=True, physicalUnits=True,
                                    numThreads=8)
-        subgrps= eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+        subgrps = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
                                       snap,
                                       "Subhalo/SubGroupNumber",
                                       noH=True, physicalUnits=True,
                                       numThreads=8)
-        birth_a= eagle_io.read_array("PARTDATA", path.replace("<type>", t),
+        birth_a = eagle_io.read_array("PARTDATA", path.replace("<type>", t),
                                       snap,
                                       "PartType4/StellarFormationTime",
                                       noH=True, physicalUnits=True,
                                       numThreads=8)
-        ini_mass= eagle_io.read_array("PARTDATA", path.replace("<type>", t),
+        ini_mass = eagle_io.read_array("PARTDATA", path.replace("<type>", t),
                                        snap,
                                        "PartType4/InitialMass",
                                        noH=True, physicalUnits=True,
                                        numThreads=8) * 10 ** 10
-        coords= eagle_io.read_array("PARTDATA", path.replace("<type>", t),
+        coords = eagle_io.read_array("PARTDATA", path.replace("<type>", t),
                                      snap,
                                      "PartType4/Coordinates",
                                      noH=True, physicalUnits=True,
                                      numThreads=8) * 1000
-        part_grps= eagle_io.read_array("PARTDATA", path.replace("<type>", t),
+        part_grps = eagle_io.read_array("PARTDATA", path.replace("<type>", t),
                                         snap,
                                         "PartType4/GroupNumber",
                                         noH=True, physicalUnits=True,
                                         numThreads=8)
-        part_subgrps= eagle_io.read_array("PARTDATA",
+        part_subgrps = eagle_io.read_array("PARTDATA",
                                            path.replace("<type>", t),
                                            snap,
                                            "PartType4/SubGroupNumber",
@@ -1318,69 +1318,69 @@ def plot_ssfr_mass_vary(snap):
                                            numThreads=8)
 
         # Apply some cuts
-        mokinds= mass > 10**8.5
-        mass= mass[mokinds]
-        cops= cops[mokinds, :]
-        grps= grps[mokinds]
-        subgrps= subgrps[mokinds]
-        hmrs= hmrs[mokinds]
+        mokinds = mass > 10**8.5
+        mass = mass[mokinds]
+        cops = cops[mokinds, :]
+        grps = grps[mokinds]
+        subgrps = subgrps[mokinds]
+        hmrs = hmrs[mokinds]
 
         # Compute the birth redshift
-        birth_z= (1 / birth_a) - 1
+        birth_z = (1 / birth_a) - 1
 
         # Get only particles born since z_100
-        okinds= birth_z < z_100
-        birth_z= birth_z[okinds]
-        ini_mass= ini_mass[okinds]
-        coords= coords[okinds, :]
-        part_grps= part_grps[okinds]
-        part_subgrps= part_subgrps[okinds]
+        okinds = birth_z < z_100
+        birth_z = birth_z[okinds]
+        ini_mass = ini_mass[okinds]
+        coords = coords[okinds, :]
+        part_grps = part_grps[okinds]
+        part_subgrps = part_subgrps[okinds]
 
         # Set up array to store sfrs
-        ssfrs= []
-        ms= []
-        plt_hmrs= []
+        ssfrs = []
+        ms = []
+        plt_hmrs = []
 
         # Loop over galaxies
         for igal in range(mass.size):
 
             # Get galaxy data
-            m= mass[igal]
-            cop= cops[igal, :]
-            g= grps[igal]
-            sg= subgrps[igal]
-            hmr= hmrs[igal]
+            m = mass[igal]
+            cop = cops[igal, :]
+            g = grps[igal]
+            sg = subgrps[igal]
+            hmr = hmrs[igal]
 
             # Get this galaxies stars
-            sokinds= np.logical_and(part_grps == g, part_subgrps == sg)
-            this_coords= coords[sokinds, :] - cop
-            this_ini_mass= ini_mass[sokinds]
+            sokinds = np.logical_and(part_grps == g, part_subgrps == sg)
+            this_coords = coords[sokinds, :] - cop
+            this_ini_mass = ini_mass[sokinds]
 
             # Compute stellar radii
-            rs= np.sqrt(this_coords[:, 0] ** 2
+            rs = np.sqrt(this_coords[:, 0] ** 2
                          + this_coords[:, 1] ** 2
                          + this_coords[:, 2] ** 2)
 
             # Get only particles within the aperture
-            rokinds= rs < 30
-            this_ini_mass= this_ini_mass[rokinds]
+            rokinds = rs < 30
+            this_ini_mass = this_ini_mass[rokinds]
 
             # Compute and store ssfr
             ssfrs.append(np.sum(this_ini_mass) / 0.1 / m)
             ms.append(m)
             plt_hmrs.append(hmr)
 
-        im= axes[ind].hexbin(ms, ssfrs, mincnt=1, gridsize=50,
+        im = axes[ind].hexbin(ms, ssfrs, mincnt=1, gridsize=50,
                               xscale="log", linewidth=0.2,
                               cmap="plasma", norm=norm, extent=extent)
-        im1= axes1[ind].hexbin(plt_hmrs, ssfrs, mincnt=1, gridsize=50,
+        im1 = axes1[ind].hexbin(plt_hmrs, ssfrs, mincnt=1, gridsize=50,
                                 xscale="log", linewidth=0.2,
                                 cmap="plasma", norm=norm, extent=extent1)
 
     # Set up colorbar
-    cbar= fig.colorbar(im, cax)
+    cbar = fig.colorbar(im, cax)
     cbar.set_label("$N$")
-    cbar1= fig1.colorbar(im1, cax1)
+    cbar1 = fig1.colorbar(im1, cax1)
     cbar1.set_label("$N$")
 
     # Save figure
