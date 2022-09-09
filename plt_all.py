79970d50df748740a9cb9eae33b1e3ccf86175ff
wjr21 <wjr21@sussex.ac.uk>
2022-09-09 14:54:29 +0100

    Calling sfr plot

--- plt_all.py
+++ plt_all.py
@@ -186,9 +186,9 @@ print("Got all data")
 # plot_ssfr_mass_size_change(data["stellar"], flares_snaps, weight_norm)
 # plot_size_sfr_evo_grid(data["stellar"], flares_snaps)
 
-# if rank == 0:
-#plot_birth_denmet_vary(flares_snaps[-1], path)
-# plot_ssfr_mass_vary(flares_snaps[-1])
+if rank == 0:
+    #plot_birth_denmet_vary(flares_snaps[-1], path)
+    plot_ssfr_mass_vary(flares_snaps[-1])
 #plot_birth_den_vary(hdf["stellar"], flares_snaps[-1], path)
 #plot_birth_met_vary(hdf["stellar"], flares_snaps[-1], path)
 # plot_size_mass_evo_grid_noncompact(hdf["stellar"], flares_snaps)
