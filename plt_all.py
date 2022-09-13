eb09e7d50d450056ba4454ea7929394daaa9dcd4
wjr21 <wjr21@sussex.ac.uk>
2022-09-13 16:37:03 +0100

    Fixing call to function

--- plt_all.py
+++ plt_all.py
@@ -183,7 +183,8 @@ print("Got all data")
 # print("Plotted SFR profiles")
 
 # PLot graph plots
-plot_ssfr_mass_size_change(data["stellar"], flares_snaps, weight_norm)
+plot_ssfr_mass_size_change(
+    hdf["stellar"], hdf["gas"], flares_snaps, weight_norm)
 # plot_size_sfr_evo_grid(data["stellar"], flares_snaps)
 
 if rank == 0:
