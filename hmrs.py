2d3c43a36779128fe4623db8e730c2e21b731428
wjr21 <wjr21@sussex.ac.uk>
2022-09-30 11:35:47 +0100

    More informative output

--- hmrs.py
+++ hmrs.py
@@ -629,8 +629,8 @@ def plot_weighted_gas_size_mass(snap, regions, weight_norm, ini_path):
             # group
             if sub_g_dens.size != ngas[igal]:
 
-                print("Galaxy (%d, %d) is incomplete, missing %d particles"
-                      % (g, sg, ngas[igal] - sub_g_dens.size))
+                print("Galaxy (%d, %d) is incomplete, missing %d particles of %d"
+                      % (g, sg, ngas[igal] - sub_g_dens.size, ngas[igal]))
 
                 # Get only this group from the raw data
                 grp_okinds = g_grps == g
