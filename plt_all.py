ecb6863e74add3a94d8a583b9aa5c503896f0bf6
wjr21 <wjr21@sussex.ac.uk>
2022-12-06 12:21:48 +0000

    Fixing binding energy calculation

--- plt_all.py
+++ plt_all.py
@@ -146,22 +146,22 @@ except OSError:
 print("Got all data")
 print(hdf["stellar"][flares_snaps[-1]].keys())
 
-if rank == 0:
-    # plot_stellar_hmr(hdf["stellar"], flares_snaps[-1], weight_norm,
-    #                  cut_on="hmr")
-    # for snap in ['028_z000p000']:
-    #     plot_eagle_stellar_hmr(snap)
-    # plot_gas_hmr(hdf["gas"], hdf["stellar"], flares_snaps[-1], weight_norm)
-    # plot_stellar_gas_hmr_comp(hdf["stellar"], hdf["gas"], flares_snaps[-1],
-    #                           weight_norm)
-    # visualise_gas(hdf["stellar"], hdf["gas"], flares_snaps[-1], path)
-    # plot_size_mass_evo_grid(hdf["stellar"], flares_snaps)
-    # plot_ssfr_mass_size_change(
-    #     hdf["stellar"], hdf["gas"], flares_snaps, weight_norm)
-    # plot_size_feedback_stellar(hdf["stellar"], hdf["stellar"],
-    #                            flares_snaps, weight_norm)
-    # plot_size_feedback_gas(hdf["gas"], hdf["stellar"],
-    #                        flares_snaps, weight_norm)
+if size == 1:
+    plot_stellar_hmr(hdf["stellar"], flares_snaps[-1], weight_norm,
+                     cut_on="hmr")
+    for snap in ['028_z000p000']:
+        plot_eagle_stellar_hmr(snap)
+    plot_gas_hmr(hdf["gas"], hdf["stellar"], flares_snaps[-1], weight_norm)
+    plot_stellar_gas_hmr_comp(hdf["stellar"], hdf["gas"], flares_snaps[-1],
+                              weight_norm)
+    visualise_gas(hdf["stellar"], hdf["gas"], flares_snaps[-1], path)
+    plot_size_mass_evo_grid(hdf["stellar"], flares_snaps)
+    plot_ssfr_mass_size_change(
+        hdf["stellar"], hdf["gas"], flares_snaps, weight_norm)
+    plot_size_feedback_stellar(hdf["stellar"], hdf["stellar"],
+                               flares_snaps, weight_norm)
+    plot_size_feedback_gas(hdf["gas"], hdf["stellar"],
+                           flares_snaps, weight_norm)
     plot_weighted_gas_size_mass(flares_snaps[-1], regions, weight_norm, path)
     plot_birth_den_vs_met(hdf["stellar"], flares_snaps[-1], weight_norm, path)
     plot_birth_met(hdf["stellar"][evo_flares_snaps[-1]],
@@ -172,7 +172,8 @@ if rank == 0:
 
     plot_size_change_blackhole(hdf["stellar"], flares_snaps, weight_norm)
 
-plot_binding_energy(hdf["stellar"], flares_snaps,
-                    weight_norm, comm, size, rank)
+else:
+    plot_binding_energy(hdf, flares_snaps,
+                        weight_norm, comm, size, rank)
 
 hdf.close()
