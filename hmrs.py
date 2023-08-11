95632f5e612abcf0356cee48373cf2659ec48225
wjr21 <wjr21@sussex.ac.uk>
2023-08-11 11:01:22 +0100

    Adjusting colors

--- hmrs.py
+++ hmrs.py
@@ -299,7 +299,7 @@ def plot_eagle_stellar_hmr(snap):
     tng_rs = np.array(tng_rs)
 
     plot_meidan_stat(tng_ms, tng_rs, np.ones(tng_rs.size),
-                     ax, "Illutris-TNG 300", "m", bin_edges)
+                     ax, "Illutris-TNG 300", "deepskyblue", bin_edges)
 
     # Plot Illutris data
     tng_ms = []
@@ -318,7 +318,7 @@ def plot_eagle_stellar_hmr(snap):
     tng_rs = np.array(tng_rs)
 
     plot_meidan_stat(tng_ms, tng_rs, np.ones(tng_rs.size),
-                     ax, "Illutris-TNG 100", "m", bin_edges, ls="--")
+                     ax, "Illutris-TNG 100", "deepskyblue", bin_edges, ls="--")
 
     # Label axes
     ax.set_xlabel("$M_\star / \mathrm{M}_\odot$")
